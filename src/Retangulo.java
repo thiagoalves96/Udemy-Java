7c219de32721b94fc65b6ffb2ea4c99c15c04db8
thiagoalves96 <thiagodev22@hotmail.com>
2023-05-09 17:29:37 -0300

    UPADTE

--- src/Retangulo.java
+++ src/Retangulo.java
@@ -9,22 +9,22 @@ public class Retangulo {
 		 
 		Scanner sc = new Scanner(System.in);
 		
-		double b,al,a,p,d;
+		double base,altura,area,perimetro,diagonal;
 		
         System.out.print("Base do retangulo: ");
-        b= sc.nextDouble();
+        base= sc.nextDouble();
         System.out.print("Altura do retangulo: ");
-        al= sc.nextDouble();
+        altura= sc.nextDouble();
         
-        a= b*al;
-        p= 2*(al+b);
-        d= Math.sqrt(Math.pow(b, 2)+Math.pow(al, 2));
+        area= base*altura;
+        perimetro= 2*(altura+base);
+        diagonal= Math.sqrt(Math.pow(base, 2)+Math.pow(altura, 2));
         
         
         
-        System.out.println("Area " + String.format("%.4f", a));
-        System.out.println("Perimetro " + String.format("%.4f", p));
-        System.out.println("Diagonal " + String.format("%.4f", d));
+        System.out.println("Area " + String.format("%.4f", area));
+        System.out.println("Perimetro " + String.format("%.4f", perimetro));
+        System.out.println("Diagonal " + String.format("%.4f", diagonal));
         
         sc.close();
 
